823a0385656515b8413841c2d3c363a4fc26822a
Leonardo Javier Alviarez Hernandez <alviarez.leonardo@gmail.com>
2019-05-02 00:54:53 -0400

    Added factories

--- accounts/factories.py
+++ accounts/factories.py
@@ -1,6 +1,9 @@
 import factory
+from factory import fuzzy
+import random
 
 from users.factories import UserFactory
+from .models import AccountItems
 
 
 class AccountsFactory(factory.django.DjangoModelFactory):
@@ -10,3 +13,27 @@ class AccountsFactory(factory.django.DjangoModelFactory):
 
     class Meta:
         model = "accounts.Accounts"
+
+
+def get_item_type():
+    "Return a random type from available choices."
+    choices = [x[0] for x in AccountItems.TYPES]
+    return random.choice(choices)
+
+
+class AccountItemsFactory(factory.django.DjangoModelFactory):
+    description = factory.Faker("text")
+    type = factory.LazyFunction(get_item_type)
+    account = factory.SubFactory(AccountsFactory)
+
+    class Meta:
+        model = "accounts.AccountItems"
+
+
+class ItemAmountsFactory(factory.django.DjangoModelFactory):
+    amount = fuzzy.FuzzyFloat(0.1, 10)
+    amount_currency = factory.Faker("currency_code")
+    account_item = factory.SubFactory(AccountItemsFactory)
+
+    class Meta:
+        model = "accounts.ItemAmounts"
